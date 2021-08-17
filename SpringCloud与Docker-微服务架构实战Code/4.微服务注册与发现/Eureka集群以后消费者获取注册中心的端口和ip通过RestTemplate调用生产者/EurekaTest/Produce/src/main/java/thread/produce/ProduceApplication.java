6776dfd05382d958cb7dce88fe03694f8f8ac55c
chuguangpeng <ThreadEmail@163.com>
2021-08-18 01:05:02 +0800

    删除多余的引用

--- SpringCloud与Docker-微服务架构实战Code/4.微服务注册与发现/Eureka集群以后消费者获取注册中心的端口和ip通过RestTemplate调用生产者/EurekaTest/Produce/src/main/java/thread/produce/ProduceApplication.java
+++ SpringCloud与Docker-微服务架构实战Code/4.微服务注册与发现/Eureka集群以后消费者获取注册中心的端口和ip通过RestTemplate调用生产者/EurekaTest/Produce/src/main/java/thread/produce/ProduceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProduceApplication {
